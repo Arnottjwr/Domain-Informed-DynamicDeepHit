fbefbcc5a97a6680e1f1065f68f2d26303f6eba3
Jack <jarnott@calypso-commodities.com>
2025-11-11 20:23:42 +0100

    deleted old main file

--- main.py
+++
@@ -1,9 +0,0 @@
-# import class to train model and run
-# just use config.json to configure settings
-
-from DynamicDeepHit.D3CKD import DomainInformedModel
-
-
-if __name__ == '__main__':
-    model = DomainInformedModel()
-    model.main()
