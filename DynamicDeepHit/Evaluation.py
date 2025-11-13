9051fc3e3d94475a294632e5f6a124265d3c9532
Jack Arnott <jack.arnott002@gmail.com>
2025-11-13 22:52:08 +0100

    plotting function working

--- DynamicDeepHit/Evaluation.py
+++ DynamicDeepHit/Evaluation.py
@@ -64,21 +64,56 @@ class Evaluation:
     #     return compute_cindex(self.Y_train_np, self.Y_test_np, self.D_train_np, self.D_test_np,\
     #                         self.events, self.duration_grid_train_np, self.cif_test_np,\
     #                             self.eval_duration_indices,self.duration_grid_test_np)
+        
+    def plot_total_loss(self, loss_dict, path, constraints = False):
+        """Plot total loss"""
+        if constraints: model_keys = ['dl1', 'dl2', 'dl3']
+        else: model_keys = ['nll', 'rank', 'longit']
 
-    def store_results(self, train_losses, val_losses, brier_scores = None, cindex_scores = None):
-        date_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-        newpath = rf'Data\\ExperiementalData\\{date_time}' 
-        if not os.path.exists(newpath):
-            os.makedirs(newpath)
-        ## TODO - make functionality to access and compare other data - have the rest of the code working 
-        ## TODO - make plotting loop for events - brier and cindex scores should be returned as dicts with events as keys
-        #------------------------------Loss Plots------------------------------#
-        fig, ax = plt.subplots() ##TODO need to plot 3 base loss functions, plus 3 domain loss functions, and the total 
-        plt.plot(train_losses, label='Training Loss')
-        plt.plot(val_losses,'--', label='Validation Loss')
-        ax.set_xlabel('Epoch')
-        ax.set_ylabel('Loss')
-        ax.set_title('Model and Domain Loss')
+        fig, ax = plt.subplots()
+        for loss_type_key in loss_dict.keys():
+            current_loss_dict = loss_dict[loss_type_key]
+            total_loss = np.sum(np.array([current_loss_dict[key] for key in model_keys]), axis = 0)
+            plt.plot(total_loss, label=f'{loss_type_key} Loss')
+            ax.set_xlabel('Epoch')
+            ax.set_ylabel('Loss')
+            ax.set_title(f'Total {loss_type_key} Loss')
+            ax.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(f'{path}/Total_{loss_type_key}_Loss_plot.png')
+        plt.close()
+
+
+    def plot_losses(self, loss_dict, path, constraints=False):
+        """Plot individual loss values with train and validation on the same plot"""
+        if constraints: model_keys = ['dl1', 'dl2', 'dl3']
+        else: model_keys = ['nll', 'rank', 'longit']
+        
+        fig, ax = plt.subplots(1, 3, figsize=(12, 7))
+        for i, key in enumerate(model_keys):
+            for loss_type_key in loss_dict.keys():  # 'train' and 'val'
+                current_loss_dict = loss_dict[loss_type_key]
+                ax[i].plot(current_loss_dict[key], label=f'{loss_type_key} {key}')
+            ax[i].set_xlabel('Epoch')
+            ax[i].set_ylabel('Loss')
+            ax[i].set_title(f'{key} Loss')
+            ax[i].grid()
+            ax[i].legend()
+        
+        plt.tight_layout()
+        plt.savefig(f'{path}/Individual_Loss_plot.png')
+        plt.close()
+    
+    
+    
+    def plot_survival_metrics(self,data):
+        fig, ax = plt.subplots()
+        plt.plot(concordance_scores['death'], label='CKD')
+        plt.plot(base_cindex,'--', label='DDH')
+        ax.set_xlabel('horizon')
+        ax.set_ylabel('score')
+        ax.set_title('Cindex')
         ax.grid()
         ax.legend()
         plt.tight_layout()
