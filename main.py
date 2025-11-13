e28529b826d91304b686360d671b3ba747e6a020
Jack Arnott <jack.arnott002@gmail.com>
2025-11-13 20:42:40 +0100

    verbosity

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from DynamicDeepHit.Evaluation import Evaluation
 
 if __name__ == '__main__':
     # Load the data
+    print('<---Starting Model--->')
+    print('Loading Data...')
     datahandler = DataPreprocessor()
     device = datahandler.device
     train_data, X_train_padded = datahandler.return_train_data()
     val_data, _ = datahandler.return_val_data()
     output_num_durations = datahandler.output_num_durations
     duration_grid_train_np = datahandler.duration_grid_train_np
-
+    print('\nData Loaded Successfully')
+    
     # Load and train the model
+    print('\nTraining Model')
     trainer = Trainer(train_data, X_train_padded, val_data, output_num_durations, device)
     model = trainer.train_and_validate()
 
